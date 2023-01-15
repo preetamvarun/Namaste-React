9ef61ce58aadcf12f1e20c739253de0b1be6bcba
Preetam Varun <preetamarun99@gmail.com>
2023-01-15 16:02:19 +0000

    done with the basic layout for navigation

--- App.js
+++ App.js
@@ -20,10 +20,10 @@ const Navigator = () => {
             <img src={require("./foodvilla.png")} alt = "Food Villa"/>
             <div>
                 <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Cart</a></li>
                 </ul>
             </div>
         </div>
